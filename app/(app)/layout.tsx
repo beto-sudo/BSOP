d4e869c8f3423b5863de89fe67c1b329b2406e5d
bsdls <beto@anorte.com>
2025-09-02 00:11:00 -0500

    Update layout.tsx

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,5 +1,6 @@
 import ThemeLoader from "../_components/ThemeLoader";
 import Sidebar from "../_components/Sidebar";
+import BuildStamp from "../_components/BuildStamp";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -8,9 +9,13 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
       <div className="flex min-h-screen">
         <Sidebar />
         <main className="flex-1 p-6">
-          <div className="mx-auto max-w-6xl">{children}</div>
+          <div className="mx-auto max-w-6xl">
+            {children}
+            <BuildStamp />
+          </div>
         </main>
       </div>
     </>
   );
 }
+
