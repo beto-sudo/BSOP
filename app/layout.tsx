27a1555d48c62a0066f80d69ce8d24584d3b0353
bsdls <beto@anorte.com>
2025-09-02 08:19:22 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,10 @@
 // app/layout.tsx
 import "./globals.css";
-import OAuthHandler from "./_components/OAuthHandler";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
-      <body>
-        {/* Maneja ?code=... globalmente */}
-        <OAuthHandler />
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
