505fc3a99b1217ec609a55f0d8ae59b348d42b02
bsdls <beto@anorte.com>
2025-09-02 08:53:29 -0500

    Update layout.tsx

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,13 +1,17 @@
 // app/(app)/layout.tsx
 import ThemeLoader from "../_components/ThemeLoader";
 import Sidebar from "../_components/Sidebar";
+import Topbar from "../_components/Topbar";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen">
       <ThemeLoader />
       <Sidebar />
-      <main className="flex-1 overflow-y-auto bg-white">{children}</main>
+      <div className="flex-1 flex flex-col bg-white">
+        <Topbar />
+        <main className="flex-1 overflow-y-auto">{children}</main>
+      </div>
     </div>
   );
 }
