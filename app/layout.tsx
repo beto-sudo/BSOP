364c162cc5cddcafaa20a8d2b4fd250a888b9a70
bsdls <beto@anorte.com>
2025-09-02 08:07:20 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,12 @@
 // app/layout.tsx
 import "./globals.css";
-import ThemeLoader from "./_components/ThemeLoader";
 import OAuthHandler from "./_components/OAuthHandler";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
       <body>
-        <ThemeLoader />
-        {/* Maneja el intercambio ?code=... una sola vez a nivel global */}
+        {/* Maneja ?code=... globalmente */}
         <OAuthHandler />
         {children}
       </body>
